38edb33afdaaa26025146a29827a6ecdfc3e0a83
ertl-hiro <ertl-hiro@32bf937e-a751-43db-81e4-e7f0be58252f>
2012-08-02 15:58:16 +0000

    バージョン番号を 1.8.1 に修正。
    git-svn-id: http://dev.toppers.jp/svn/cfg/trunk@94 32bf937e-a751-43db-81e4-e7f0be58252f

--- cfg/cfg_version.cpp
+++ cfg/cfg_version.cpp
@@ -41,7 +41,7 @@
 #include <sys/stat.h>
 
 //! cfgのバージョン情報
-extern char const cfg_version[] = "1.8.0";
+extern char const cfg_version[] = "1.8.1";
 
 /*!
  *  \brief  プログラムファイル(cfg or cfg.exe)のタイムスタンプを取得する。
