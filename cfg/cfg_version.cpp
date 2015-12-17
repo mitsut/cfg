cc544667f67e3bc1485d3769d4de4b37416cc02d
nces-mtakada <nces-mtakada@32bf937e-a751-43db-81e4-e7f0be58252f>
2015-12-17 02:32:21 +0000

    バージョン1.9.5用のリリース対応
    git-svn-id: http://dev.toppers.jp/svn/cfg/trunk@195 32bf937e-a751-43db-81e4-e7f0be58252f

--- cfg/cfg_version.cpp
+++ cfg/cfg_version.cpp
@@ -41,7 +41,7 @@
 #include <sys/stat.h>
 
 //! cfgのバージョン情報
-extern char const cfg_version[] = "1.9.4";
+extern char const cfg_version[] = "1.9.5";
 
 /*!
  *  \brief  プログラムファイル(cfg or cfg.exe)のタイムスタンプを取得する。
